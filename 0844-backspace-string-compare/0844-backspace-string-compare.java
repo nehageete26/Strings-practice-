2adfa0d2a15fe6419e8cc0143d8e7e2ef327567f
Neha Geete <nehageete92@gmail.com>
2026-07-04 14:56:23 +0530

    Time: 1 ms (80.74%), Space: 42.7 MB (94.32%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,51 +1,46 @@
-// class Solution {
-//     private String getactualstring(String input){
-//         int hash = 0;
-//         StringBuilder actualstring = new StringBuilder();
-//         for(int i=input.length()-1;i>=0;i--){
-//             if(input.charAt(i) == '#'){
-//                 hash++;
-//                 continue;
-//             }
-
-//             if(hash >0){
-//                 hash--;
-//             }
-//             else actualstring.append(input.charAt(i));
-//         }
-//         return actualstring.toString();
-//     }
-//     public boolean backspaceCompare(String s, String t) {
-//         return getactualstring(s).equals(getactualstring(t));
-//     }
-// }
-import java.util.Stack;
-
 class Solution {
-
-    public String backspaceRemove(String str) {
-        Stack<Character> st = new Stack<>();
-
-        for (char ch : str.toCharArray()) {
-            if (ch != '#') {
-                st.push(ch);
-            } else {
-                if (!st.isEmpty()) {
-                    st.pop();
-                }
+    private String getactualstring(String input){
+        int hash = 0;
+        StringBuilder actualstring = new StringBuilder();
+        for(int i=input.length()-1;i>=0;i--){
+            if(input.charAt(i) == '#'){
+                hash++;
+                continue;
             }
+
+            if(hash >0){
+                hash--;
+            }
+            else actualstring.append(input.charAt(i));
         }
-
-        StringBuilder res = new StringBuilder();
-
-        for (char ch : st) {
-            res.append(ch);
-        }
-
-        return res.toString();
+        return actualstring.toString();
     }
-
     public boolean backspaceCompare(String s, String t) {
-        return backspaceRemove(s).equals(backspaceRemove(t));
+        return getactualstring(s).equals(getactualstring(t));
     }
 }
+
+// we can also use stack 
+// class Solution {
+//     public String backspaceRemove(String str) {
+//         Stack<Character> st = new Stack<>();
+//         for (char ch : str.toCharArray()) {
+//             if (ch != '#') {
+//                 st.push(ch);
+//             } else {
+//                 if (!st.isEmpty()) {
+//                     st.pop();
+//                 }
+//             }
+//         }
+//         StringBuilder res = new StringBuilder();
+//         for (char ch : st) {
+//             res.append(ch);
+//         }
+
+//         return res.toString();
+//     }
+//     public boolean backspaceCompare(String s, String t) {
+//         return backspaceRemove(s).equals(backspaceRemove(t));
+//     }
+// }
