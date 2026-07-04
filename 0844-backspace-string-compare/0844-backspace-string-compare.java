81099a8b12defcb74cadf3e59a9bc4c725839fd6
Neha Geete <nehageete92@gmail.com>
2026-07-04 14:54:36 +0530

    Time: 2 ms (56.89%), Space: 43.2 MB (37.95%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,23 +1,51 @@
-class Solution {
-    private String getactualstring(String input){
-        int hash = 0;
-        StringBuilder actualstring = new StringBuilder();
-        for(int i=input.length()-1;i>=0;i--){
-            if(input.charAt(i) == '#'){
-                hash++;
-                continue;
-            }
+// class Solution {
+//     private String getactualstring(String input){
+//         int hash = 0;
+//         StringBuilder actualstring = new StringBuilder();
+//         for(int i=input.length()-1;i>=0;i--){
+//             if(input.charAt(i) == '#'){
+//                 hash++;
+//                 continue;
+//             }
 
-            if(hash >0){
-                hash--;
+//             if(hash >0){
+//                 hash--;
+//             }
+//             else actualstring.append(input.charAt(i));
+//         }
+//         return actualstring.toString();
+//     }
+//     public boolean backspaceCompare(String s, String t) {
+//         return getactualstring(s).equals(getactualstring(t));
+//     }
+// }
+import java.util.Stack;
+
+class Solution {
+
+    public String backspaceRemove(String str) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch != '#') {
+                st.push(ch);
+            } else {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
             }
-            else actualstring.append(input.charAt(i));
         }
-        return actualstring.toString();
+
+        StringBuilder res = new StringBuilder();
+
+        for (char ch : st) {
+            res.append(ch);
+        }
+
+        return res.toString();
     }
 
-
     public boolean backspaceCompare(String s, String t) {
-        return getactualstring(s).equals(getactualstring(t));
+        return backspaceRemove(s).equals(backspaceRemove(t));
     }
 }
