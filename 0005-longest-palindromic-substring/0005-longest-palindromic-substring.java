78ba66edd3b9798ebfa67937352d0f3e582d83f3
Neha Geete <nehageete92@gmail.com>
2026-07-07 21:03:14 +0530

    Time: 19 ms (41.07%), Space: 47.1 MB (17.57%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,56 @@
-class Solution {
-    public boolean ispalindrome(String s , int i, int j){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;
-            j--; 
-        }
-        return true;
-    }
-    public String longestPalindrome(String s) {
-        int maxi = 0, start = 0, end = 0;
-          for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(ispalindrome(s,i,j) == true){
-                    if((j-i+1) > maxi){
-                    maxi = j-i+1;
-                    start = i;
-                    end = j;
-                    }
-                }
+// class Solution {
+//     public boolean ispalindrome(String s , int i, int j){
+//         while(i<j){
+//             if(s.charAt(i)!=s.charAt(j)) return false;
+//             i++;
+//             j--; 
+//         }
+//         return true;
+//     }
+//     public String longestPalindrome(String s) {
+//         int maxi = 0, start = 0, end = 0;
+//           for(int i=0;i<s.length();i++){
+//             for(int j=i;j<s.length();j++){
+//                 if(ispalindrome(s,i,j) == true){
+//                     if((j-i+1) > maxi){
+//                     maxi = j-i+1;
+//                     start = i;
+//                     end = j;
+//                     }
+//                 }
+//             }
+//           }
+//           return s.substring(start , end+1);
+//     }
+// }
+// the above code complexity is O(N^3) which is worst , you can solve using two pointers and manacher whatever for opti
+class Solution{
+    public String longestPalindrome(String s){
+        int n = s.length();
+        String longest_palindrome_substring = "";
+        if(n <= 1) return s;
+        int low = 0, high = 0;
+        // for odd length -
+        for(int i=0;i<n;i++){
+            low = i;
+            high = i;
+            while(low >=0 && high < n && s.charAt(low)==s.charAt(high)){
+                low --;
+                high++;
             }
-          }
-          return s.substring(start , end+1);
+                String palindrome_string = s.substring(low+1,high);
+                if(palindrome_string.length()>longest_palindrome_substring.length()) longest_palindrome_substring = palindrome_string;
+            
+            // even length -
+            low = i-1;
+            high = i;
+                 while(low >=0 && high < n && s.charAt(low)==s.charAt(high)){
+                low --;
+                high++;
+                }
+                palindrome_string = s.substring(low+1,high);
+                if(palindrome_string.length()>longest_palindrome_substring.length()) longest_palindrome_substring = palindrome_string;
+            }
+        return longest_palindrome_substring;
     }
 }
