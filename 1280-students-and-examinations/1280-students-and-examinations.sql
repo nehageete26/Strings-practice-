2565c847dad406b7a1a9e0e727f37a889d2836a1
Neha Geete <nehageete92@gmail.com>
2026-07-07 19:22:47 +0530

    Time: 426 ms (32.19%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -3,7 +3,6 @@ select s.student_id , s.student_name, sub.subject_name , count(e.subject_name) a
 from students s
 cross join subjects sub
 left join examinations e 
-on s.student_id = e.student_id
-and sub.subject_name = e.subject_name
+on s.student_id = e.student_id and sub.subject_name = e.subject_name
 group by s.student_name , sub.subject_name, s.student_id
 order by s.student_id , sub.subject_name;
