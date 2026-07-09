46c55a644689b65a08bcb831a8481f54028a8ce5
Neha Geete <nehageete92@gmail.com>
2026-07-09 17:58:28 +0530

    Time: 137 ms (5.79%), Space: 46.9 MB (5.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,32 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i); 
+//             if(ch=='('|| ch=='{'|| ch=='['){
+//                 stack.push(ch);
+//             }
+//             else{
+//             if(stack.isEmpty()) return false;
+        
+//         char top = stack.pop();
+
+//         if(ch==')' && top != '(' || ch=='}' && top != '{' || ch==']' && top != '[' ) return false;
+//         }
+//         }
+//         return stack.isEmpty();
+//     }
+// }
+
+// without using stack
+class Solution{
+    public boolean isValid(String s){
+while(true){
+    if(s.contains("()")) s = s.replace("()","");
+    else if (s.contains("{}")) s = s.replace("{}","");
+    else if (s.contains("[]")) s = s.replace("[]","");
+    else 
+    return s.isEmpty();
+}
+    }
+}
