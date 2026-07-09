cf388e4051195e850c37aa336dc30fe78a891a5b
Neha Geete <nehageete92@gmail.com>
2026-07-10 02:09:04 +0530

    Time: 18 ms (9.5%), Space: 44.5 MB (96.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // s = "abcabcbb"
-        int arr[] = new int[256];
-        for(int i=0;i<arr.length;i++){
-            arr[i] = -1;
-        }
-        int left = 0 , right = 0 , maxlength = 0;
-        while(right < s.length()){
-            if(arr[s.charAt(right)] != -1){ // arr['a'] = 0
-                if(arr[s.charAt(right)]>= left){ // 0 >= 0
-                // When a repeated character comes inside the current window, move left to : 
-                    left = arr[s.charAt(right)]+1; // left = 1 
-                }
+        ArrayList<Character> list = new ArrayList<>();
+        int start = 0, end = 0, max_length = 0;
+        while(end < s.length()){
+            while(list.contains(s.charAt(end))&& start < s.length()){
+                list.remove(Character.valueOf(s.charAt(start)));
+                start ++;
             }
-            int length = right - left + 1;
-            maxlength = Math.max(maxlength,length);
-            
-            // here we store index arr['a'] = 0 then r++ , arr['b'] = 1 , arr['c'] = 2, arr['a'] = 3
-            arr[s.charAt(right)] = right;
-            right ++;
+            list.add(s.charAt(end));
+            max_length = Math.max(max_length,list.size());
+            end ++;
         }
-        return maxlength;
+        return max_length;
     }
 }
