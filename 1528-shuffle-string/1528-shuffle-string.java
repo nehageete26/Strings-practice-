a7c7e152ac563ba34a169eae378b6c11fbda36c2
Neha Geete <nehageete92@gmail.com>
2026-07-31 22:00:26 +0530

    Time: 4 ms (8.63%), Space: 46.7 MB (5.56%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        HashMap<Integer,Character> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+        while(i < s.length() && j < indices.length){
+            map.put(indices[j],s.charAt(i));
+            i++;
+            j++;
+        }
+        for(int k=0;k<map.size();k++){
+           sb.append(map.get(k));
+        }
+        return sb.toString();
+    }
+}
