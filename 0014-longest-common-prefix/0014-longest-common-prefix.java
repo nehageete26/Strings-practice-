01ec8d7ee5b42d02904fa4089d39104882cff8b7
Neha Geete <nehageete92@gmail.com>
2026-09-05 22:22:07 +0530

    Time: 1 ms (60.35%), Space: 43.4 MB (32.21%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        StringBuilder sb = new StringBuilder();
+        String str1 = strs[0];
+        String str2 = strs[strs.length-1];
+        int i = 0;
+        while(i < str1.length()){
+            if(str1.charAt(i) == str2.charAt(i)) i++;
+            else break;
+        }
+        return str1.substring(0,i);
+    }
+}
