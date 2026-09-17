ea38ddaa9815b532c22aa4d9fea8e72ada50e31b
Neha Geete <nehageete92@gmail.com>
2026-09-18 02:27:15 +0530

    Time: 1 ms (99.84%), Space: 46.5 MB (95.83%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+        if(i%3==0 && i%5==0) list.add("FizzBuzz");
+        else if(i%3==0) list.add("Fizz");
+        else if(i%5==0) list.add("Buzz");
+        else {
+            String str = String.valueOf(i); 
+            list.add(str);
+        }
+        }
+        return list;
+    }
+}
